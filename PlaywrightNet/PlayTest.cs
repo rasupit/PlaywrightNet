3999ebf67fb15a646210fba19f38868f69d58c96
Ricky Supit <rasupit@gmail.com>
2023-06-17 08:12:22 +0800

    Add project files.

---
+++ PlaywrightNet/PlayTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightNet
+{
+    static internal class PlayTest
+    {
+        public static async Task BrowseAsync()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            await page.ScreenshotAsync(new()
+            {
+                Path = "screenshot.png"
+            });
+
+        }
+    }
+}
---
+++ PlaywrightNet/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+// .NET information
+using Microsoft.Playwright;
+using PlaywrightNet;
+using System.Net;
+using System.Runtime.InteropServices;
+using static System.Console;
+
+WriteLine(RuntimeInformation.FrameworkDescription);
+
+// OS information
+const string OSRel = "/etc/os-release";
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) &&
+    File.Exists(OSRel))
+{
+    const string PrettyName = "PRETTY_NAME";
+    foreach (string line in File.ReadAllLines(OSRel))
+    {
+        if (line.StartsWith(PrettyName))
+        {
+            ReadOnlySpan<char> value = line.AsSpan()[(PrettyName.Length + 2)..^1];
+            WriteLine(value.ToString());
+            break;
+        }
+    }
+}
+else
+{
+    WriteLine(RuntimeInformation.OSDescription);
+}
+
+WriteLine();
+
+const long Mebi = 1024 * 1024;
+const long Gibi = Mebi * 1024;
+GCMemoryInfo gcInfo = GC.GetGCMemoryInfo();
+long totalMemoryBytes = gcInfo.TotalAvailableMemoryBytes;
+
+// Environment information
+WriteLine($"{nameof(Environment.UserName)}: {Environment.UserName}");
+WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
+WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
+WriteLine($"{nameof(GCMemoryInfo.TotalAvailableMemoryBytes)}: {totalMemoryBytes} ({GetInBestUnit(totalMemoryBytes)})");
+WriteLine($"HostName : {Dns.GetHostName()}");
+
+PlayTest.BrowseAsync().GetAwaiter().GetResult();
+
+//Console.Read();
+
+string GetInBestUnit(long size)
+{
+    if (size < Mebi)
+    {
+        return $"{size} bytes";
+    }
+    else if (size < Gibi)
+    {
+        double mebibytes = (double)(size / Mebi);
+        return $"{mebibytes:F} MiB";
+    }
+    else
+    {
+        double gibibytes = (double)(size / Gibi);
+        return $"{gibibytes:F} GiB";
+    }
+}
+
